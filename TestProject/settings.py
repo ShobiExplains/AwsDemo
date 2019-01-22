a551df8c58b6feac4195220eb579ec27ecb4c3d1
Shobi P P <ppshobi@gmail.com>
2019-01-23 00:02:49 +0530

    added allowed hosts

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p*kmcoj2o4%$*x^m7u)rs20$j1@8=ca!6ejgw!-6v9pgk+@&-s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
